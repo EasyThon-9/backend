510ca49f39586d5df5bbc59f8b3c3c6297acd7d2
DHKim7762 <149556729+ehdgusdl@users.noreply.github.com>
2025-11-21 01:36:42 +0900

    celery, RabbitMQ 세팅

---
+++ app/core/celery_app.py
@@ -0,0 +1,18 @@
+from celery import Celery
+from app.core.config import settings
+
+# 1. Celery 인스턴스 생성
+celery_app = Celery(
+    "worker",
+    broker=settings.CELERY_BROKER_URL,
+    backend=settings.CELERY_RESULT_BACKEND
+)
+
+# 2. 설정 적용
+celery_app.conf.update(
+    task_serializer="json",
+    result_serializer="json",
+    accept_content=["json"],
+    timezone="Asia/Seoul",
+    enable_utc=True,
+)
